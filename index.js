6ea754dd9f8e225f3b2b3ae181b83d04b05c60ab
ShreeroopN <100685797+ShreeroopN@users.noreply.github.com>
2022-09-29 23:47:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// $(".navbar").hide();
+// $(".top-section").focus(function(){
+//   console.log("Hiuas");
+//   $(".navbar").slideDown();
+// })
+// // $(".top-section").on("mouseenter",function(){
+// //   $(".navbar").slideUp();
+// // })
